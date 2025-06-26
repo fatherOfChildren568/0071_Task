00ae548ae8d71428853eaa47bbe915111913237d
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-06-27 02:51:46 +0700

    upadte Message.java

--- constant/Message.java
+++ constant/Message.java
@@ -9,5 +9,17 @@ public class Message {
 
     public static final String ERROR_LIMIT_CHOICE = "Please input in range %s -> %s";
     public static final String ERROR_INVALID_INPUT_INT = "Invalid choice";
+    public static final String PROMPT_TIME_START = "Time start: ";
+    public static final String PROMPT_TIME_END = "Time start: ";
+    public static final String PROMT_NAME_TASK = "Name task: ";
+    public static final String PROMPT_TASK_TYPE = "Task type: ";
+    public static final String PROMPT_ASSIGNEE = "Assignee: ";
+    public static final String PROMT_REVIEWER = "Reviewer: ";
+
     public static final String ERROR_INPUT_EMPTY = "String cannot be empty";
+    public static final String ERROR_LIMIT_TIME = "Time limit from %s --> %s";
+    public static final String ERROR_TIME_FORMART = "Input time is error formart";
+
+    public static final String HEADER_LSIT = String.format("%-5s%-15s%-15s%-15s%-12s%-12s%-12s%-12s\n",
+            "ID", "Name Task", "Type Task", "Date", "From", "To", "Assignee", "Reviewer");
 }
